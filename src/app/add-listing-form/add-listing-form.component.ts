749f8eb5d5664ec35aae85a41aff48f0f3696e91
Justyna Foster <justynawfoster@gmail.com>
2017-08-02 17:15:13 -0600

    create a form

--- src/app/add-listing-form/add-listing-form.component.ts
+++ src/app/add-listing-form/add-listing-form.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-add-listing-form',
+  selector: 'app-add-listing-form', //add to app.template
   templateUrl: './add-listing-form.component.html',
   styleUrls: ['./add-listing-form.component.css']
 })
 export class AddListingFormComponent implements OnInit {
 
+  propertyTypes: Array<string> = ['House', 'Condo', 'Duplex'];
   constructor() { }
 
   ngOnInit() {
   }
 
+  onCribSubmit(data): void { //void - not returning anything from these method
+    console.log(data); 
+  }
 }
