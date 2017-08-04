698f9317b32cba5a432ad07d88498c89a782bf1e
Justyna Foster <justynawfoster@gmail.com>
2017-08-04 15:01:51 -0600

    listing new forms

--- src/app/crib-listing/crib-listing.component.ts
+++ src/app/crib-listing/crib-listing.component.ts
@@ -23,6 +23,11 @@ export class CribListingComponent implements OnInit {
         data => this.cribs = data, 
         error => this.error = error.statusText
       );
-  }
 
+      this.cribsService.newCribSubject.subscribe( //this is where we subscribe our subject
+        // data => console.log(data) //if we get the data console.log the data
+      // data => this.cribs.push(data) adding at the end of the list
+        data => this.cribs = [data, ...this.cribs]
+      )   
+  }
 }
--- src/app/services/cribs.service.ts
+++ src/app/services/cribs.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import 'rxjs/add/operator/map'; 
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/map'; //library that let us use observables in angular app
 
 @Injectable()
 export class CribsService {
+
+  public newCribSubject = new Subject<any>();
 
   constructor(private http: Http) { }
 
   getAllCribs(){
     return this.http.get('data/cribs.json').map(res => res.json());
-  }
+  } //returns observables and because of that we can subscribe them results (look in crib-listing)
 
+  addCrib(data){
+    data.image = 'default-crib';
+    this.newCribSubject.next(data); //whenever the data will come from the addCrib method; whenever button is clicked and 
+    //this method is called send data to this Subject
+
+  }
 }
